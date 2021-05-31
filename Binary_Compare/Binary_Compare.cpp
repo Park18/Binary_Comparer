efabbaaf927f7c8ae7a240e3152b2dfa6daf64e5
Park18 <sowoomi35@gmail.com>
2021-05-31 18:38:58 +0900

    update comment

--- Binary_Compare/Binary_Compare.cpp
+++ Binary_Compare/Binary_Compare.cpp
@@ -6,30 +6,17 @@
 
 using namespace std;
 
+// argv[1] - 원본 디렉토리
+// argv[2] - 변조 디렉토리
+// argv[3] - 로그 파일명
 int main(int argc, char** argv)
-{
-    //string origin_file("D:\\Backup\\대학교\\학과\\4학년\\캡스톤\\원본-감염파일\\sample\\000000.swf");
-    //string origin_hash = Fuzzy_System::fuzzy_hash_compute(origin_file);
-    //cout << "original: " << origin_hash << endl;
-    //
-    //string modula_file("D:\\Backup\\대학교\\학과\\4학년\\캡스톤\\원본-감염파일\\GANDCRAB RANSOMWARE INFECTION (VERSION 5.0.4)-2018-11-02\\000000.swf.hhxjs");
-    //string modula_hash = Fuzzy_System::fuzzy_hash_compute(modula_file);
-    //cout << "original: " << modula_hash << endl;
-    //
-    //unsigned int score = Fuzzy_System::fuzzy_hash_comppare(origin_hash, modula_hash);
-    //cout << "score: " << score << endl;
-
-    // bfs::path file("000000.swf.hhxjs");
-    // cout << file.filename().string() << endl;    // 파일 확장자 포함 파일 이름
-    // cout << file.extension() << endl;   // 파일 확장자
-    // cout << file.stem() << endl;        // 파일 확장자 제외 파일 이름
-    
+{   
     try
     {
         clock_t start = clock();
 
-        Comparer comparer(argv[1], argv[2], argv[3]);
-        comparer.run();
+        Comparer comparer(argv[1], argv[2]);
+        comparer.run(argv[3]);
     
         clock_t end = clock();
     
